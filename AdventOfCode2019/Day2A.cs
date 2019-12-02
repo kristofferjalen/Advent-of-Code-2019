fc935258ed0569cc89a141d180195dee084552b4
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2019-12-02 19:50:36 +0100

    Initial commit

---
+++ AdventOfCode2019/Day2A.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2A
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllText("input/02A-input.txt");
+
+            var z = input.Split(',').Select(int.Parse).ToArray();
+
+            z[1] = 12;
+            z[2] = 2;
+
+            var i = 0;
+            while (true)
+            {
+                if (z[i] == 1)
+                    z[z[i + 3]] = z[z[i + 1]] + z[z[i + 2]];
+                else if (z[i] == 2)
+                    z[z[i + 3]] = z[z[i + 1]] * z[z[i + 2]];
+                else if (z[i] == 99)
+                    break;
+
+                i += 4;
+            }
+            
+            Console.WriteLine(z[0]); // 5290681
+        }
+    }
+}
---
+++ AdventOfCode2019/Day2B.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day2B
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllText("input/02A-input.txt");
+
+            var x = 0;
+
+            var z = new int[input.Length];
+
+            while (x <= 99)
+            {
+                var y = 0;
+
+                while (y <= 99)
+                {
+                    z = input.Split(',').Select(int.Parse).ToArray();
+                    z[1] = x;
+                    z[2] = y++;
+
+                    var i = 0;
+                    do
+                    {
+                        if (z[0] == 19690720)
+                        {
+                            x = y = 100;
+                            break;
+                        }
+                        else if (z[i] == 1)
+                            z[z[i + 3]] = z[z[i + 1]] + z[z[i + 2]];
+                        else if (z[i] == 2)
+                            z[z[i + 3]] = z[z[i + 1]] * z[z[i + 2]];
+                        else
+                            break;
+                    }
+                    while ((i += 4) < z.Length);
+                }
+                x++;
+            }
+
+            Console.WriteLine(100 * z[1] + z[2]); // 5741
+        }
+    }
+}
