58be882a7b2b4e9d6dc80bf005f5be3ed479a75d
Kristoffer Jälén <kristoffer.jalen@tretton37.com>
2019-12-04 20:20:33 +0100

    Add day 4

---
+++ AdventOfCode2019/Day4A.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day4A
+{
+    class Program
+    {
+        static void Main()
+        {
+            var count = 0;
+
+            for (var curr = 272091; curr <= 815432; curr++)
+            {
+                var s = $"{curr}";
+
+                var increasing = true;
+                var adjacent = false;
+                var i = 0;
+                while (i < s.Length - 1)
+                {
+                    increasing &= s[i + 1] >= s[i];
+                    adjacent   |= s[i + 1] == s[i];
+                    i++;
+                }
+
+                count += increasing && adjacent ? 1 : 0;
+            }
+
+            Console.WriteLine(count); // 931
+        }
+    }
+}
---
+++ AdventOfCode2019/Day4B.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Day4B
+{
+    class Program
+    {
+        static void Main()
+        {
+            var count = 0;
+
+            for (var curr = 272091; curr <= 815432; curr++)
+            {
+                var s = $"{curr}";
+
+                // Check if increasing
+                var increasing = s
+                    .Select((c, i) => (i < s.Length - 1 && s[i + 1] >= s[i] || i == s.Length - 1))
+                    .All(x => x);
+                
+                // Check if two adjacent
+                var i = 0;
+                var twoAdjacent = false;
+                while (!twoAdjacent && i < s.Length)
+                {
+                    var j = i;
+                    while (j < s.Length && s[j] == s[i])
+                        j++;
+
+                    twoAdjacent = j - i == 2;
+                    i = j;
+                }
+
+                count += increasing && twoAdjacent ? 1 : 0;
+            }
+
+            Console.WriteLine(count); // 609
+        }        
+    }
+}
